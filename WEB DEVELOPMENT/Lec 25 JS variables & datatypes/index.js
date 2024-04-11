60654f13f6793a6e0582cfa1a7e6c7a19ba5f3e4
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-04-11 18:00:30 +0530

    Added content

---
+++ WEB DEVELOPMENT/Lec 25 JS variables & datatypes/index.js
@@ -0,0 +1,27 @@
+// var age = 25;
+
+// if(true) {
+//     console.log(age);
+// }
+
+function solve() {
+    var age = 25;
+    console.log(age);
+}
+
+// console.log(age);
+solve();
+
+// blocked scoped
+{
+    let a = 18;
+    console.log(a);
+}
+
+
+// Dynamically typed language -> ek hi variable ke ander multiple types value insert kar skte hai.
+let a = 43;
+a = "vaibhav";
+a = true;
+a = null;
+
