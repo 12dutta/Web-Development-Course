bc73659ae11a986b245d75a415f2db9ba6501f34
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-04-12 18:14:08 +0530

    update

---
+++ WEB DEVELOPMENT/Lec 26 JS operator & conditional/index.js
@@ -0,0 +1,18 @@
+// console.log("vaibhav");
+
+// Arithmetic operator-> +,  -,  *,  /,  %,  **
+let a = 10;
+let b = 2;
+// console.log(a**b);  
+
+// let c = 4;
+// console.log(c++);   
+
+// assignment operator
+b -= 2;
+console.log(b);
+
+
+
+
+
