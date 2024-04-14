cb077a5c6fef610d026a2ce2ab5e1adeab4f1b93
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-04-14 16:51:26 +0530

    update

--- WEB DEVELOPMENT/Lec 27 JS loops & string/style.js
+++ WEB DEVELOPMENT/Lec 27 JS loops & string/style.js
@@ -1,1 +1,13 @@
-console.log("vaibhav dutta");
+// console.log("vaibhav dutta");
+// for loop
+// for(let i=1; i<=10; i++){
+//     console.log("Vaibhav");
+// }
+
+// for( let i=0; i<=5; i++) {
+//     console.log(i);
+// }
+
+for( let i=5; i>=5; i--) {
+    console.log(i);
+}
