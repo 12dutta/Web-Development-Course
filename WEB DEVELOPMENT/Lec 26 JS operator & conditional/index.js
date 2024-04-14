7c0e86a6966b2b9a0ee92fceea2e69d773d43165
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-04-14 16:18:09 +0530

    update

--- WEB DEVELOPMENT/Lec 26 JS operator & conditional/index.js
+++ WEB DEVELOPMENT/Lec 26 JS operator & conditional/index.js
@@ -19,13 +19,30 @@ let myage = 11;
 // (myage >= 18) ? (console.log("Can Drive")) : (console.log("Cannot Drive"));
 
 // non boolean logical operator
-console.log(false || 'Dutta');
-console.log(false || '3');
-console.log(false || '0.8');
+// console.log(false || 'Dutta');
+// console.log(false || '3');
+// console.log(false || '0.8');
 
 // Bitwise operator
-console.log(2 | 5);
-console.log(~(0));
-console.log(2 ^ 2);
+// console.log(2 | 5);
+// console.log(~(0));
+// console.log(2 ^ 2);
+// console.log(10 << 3);
+
+// conditionals-> if, if-else, if-else-if, switch
+let num = 6;
+switch(num){
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    case 4: console.log('D');
+    break;
+    default: console.log('F');
+}
+
+
 
 
