8c24112a594347b9f66504a743b3aa12c783e645
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-04-26 17:07:15 +0530

    Added

--- WEB DEVELOPMENT/Lec 28 JS Functions/script.js
+++ WEB DEVELOPMENT/Lec 28 JS Functions/script.js
@@ -16,4 +16,18 @@ function printNumber(num) {
 }
 printNumber(34);
 
+// function with multiple parameter
+// function getAverage(num1, num2) {
+//     let avg = (num1 + num2)/2;
+//     console.log("Average: ", avg);
+// }
+// getAverage(3,70);
+
+// Return functions
+function getSum(a,b,c) {
+    let sum = a+b+c;
+    return sum;
+}
+let ans = getSum(1,2,3);
+console.log("printing sum: ", ans);
 
