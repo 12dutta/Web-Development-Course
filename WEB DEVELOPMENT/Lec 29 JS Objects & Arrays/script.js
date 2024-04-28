de7277859045c951241a0d5aea4ef1e22b5dff3c
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-04-28 18:05:21 +0530

    Added

--- WEB DEVELOPMENT/Lec 29 JS Objects & Arrays/script.js
+++ WEB DEVELOPMENT/Lec 29 JS Objects & Arrays/script.js
@@ -1,12 +1,25 @@
-let obj = {
-    name: "Vaibhav",
-    age: 22,
-    weight: 76,
-    height: "5ft 7inch",
-    greet: function() {
-        console.log("Hello Guys");
-    }
-};
-console.log(obj);
-obj.greet();
-console.log(typeof(obj));
+// let obj = {
+//     name: "Vaibhav",
+//     age: 22,
+//     weight: 76,
+//     height: "5ft 7inch",
+//     greet: function() {
+//         console.log("Hello Guys");
+//     }
+// };
+// console.log(obj);
+// obj.greet();
+// console.log(typeof(obj));
+
+//creation of arrays
+let arr = [1,2,3,4,5];
+//Arrays constructor
+let brr = new Array('Vaibhav', 22, false);
+brr.push('Dutta');
+brr.push('25');
+brr.push('30');
+// brr.pop();
+// brr.shift();
+brr.unshift('Name');
+console.log(brr.slice(2,5));
+console.log(brr);
