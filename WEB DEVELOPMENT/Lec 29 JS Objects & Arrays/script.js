5ffcb8cbf81543ed7466a7f228468aef61ddb655
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-04-28 16:36:54 +0530

    Added

---
+++ WEB DEVELOPMENT/Lec 29 JS Objects & Arrays/script.js
@@ -0,0 +1,11 @@
+let obj = {
+    name: "Vaibhav",
+    age: 22,
+    weight: 76,
+    height: "5ft 7inch",
+    greet: function() {
+        console.log("Hello Guys");
+    }
+};
+console.log(obj);
+obj.greet();
