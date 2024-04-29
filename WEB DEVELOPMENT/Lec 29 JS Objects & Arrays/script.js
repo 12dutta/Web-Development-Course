b50337f765c8f52894a48c98f53ea84e60d4bccf
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-04-29 16:59:48 +0530

    Update script.js

--- WEB DEVELOPMENT/Lec 29 JS Objects & Arrays/script.js
+++ WEB DEVELOPMENT/Lec 29 JS Objects & Arrays/script.js
@@ -35,3 +35,27 @@ brr.splice(1,3, 'Bablu');
 //     return number*number;
 // })
 // console.log(ansArray);
+
+//Filter function
+let arr = [10,20,30,11,43,24,51,38];
+let evenArray = arr.filter((number) => {
+    return number%2 === 0;
+    // if(number%2 === 0) {
+    //     return true;
+    // }
+    // else {
+    //     return false;
+    // }
+});
+// console.log(evenArray);
+let crr = [1,3,'vaibhav','Bablu',true,null];
+let string = crr.filter((value) => {
+    // return (typeof(value) === 'string');
+    if(typeof(value) == 'string'){
+        return true;
+    }
+    else {
+        return false;
+    }
+});
+console.log(string);
