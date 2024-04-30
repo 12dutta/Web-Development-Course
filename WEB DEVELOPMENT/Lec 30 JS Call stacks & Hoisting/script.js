2896cfb5da2a2ebc271b4fc12d8363cc8f78ab88
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-04-30 18:31:41 +0530

    Added

--- WEB DEVELOPMENT/Lec 30 JS Call stacks & Hoisting/script.js
+++ WEB DEVELOPMENT/Lec 30 JS Call stacks & Hoisting/script.js
@@ -1,6 +1,27 @@
-//Hoisting function
-sayMyName("Vaibhav");
+//Var Hoisting
+// console.log(age);
+// var age = 22;
 
-function sayMyName(finalName) {
-    console.log(finalName);
+
+//Hoisting function
+// sayMyName("Vaibhav");
+
+// function sayMyName(finalName) {
+//     console.log(finalName);
+// }
+
+//using let and const -> not work var hoisting
+let age = 22;
+console.log(age);
+
+
+//using function expression -> not work function hoisting
+// sayHello();
+// let sayHello = function() {
+//     console.log("Hello Friends");
+// }
+
+//class hoisting
+class Human {
+
 }
