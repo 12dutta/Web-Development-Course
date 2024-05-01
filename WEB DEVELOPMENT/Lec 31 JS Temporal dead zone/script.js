a42bfacae017798f44ec5005d621c79f50dec629
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-05-01 17:42:18 +0530

    Added

--- WEB DEVELOPMENT/Lec 31 JS Temporal dead zone/script.js
+++ WEB DEVELOPMENT/Lec 31 JS Temporal dead zone/script.js
@@ -1,23 +1,35 @@
 
 //Global scope -> isme agar varible create karte h koi bhi keyword(let, var, const) se toh usse kahi bhi access kar skte h
-var age = 22;
-console.log(age);
+// var age = 22;
+// console.log(age);
 
-{
-    console.log(age);
-}
+// {
+//     console.log(age);
+// }
 
-if(true) {
-    console.log(age);
-}
+// if(true) {
+//     console.log(age);
+// }
 
-for(let i=0; i<2; i++) {
-    console.log(age);
-}
+// for(let i=0; i<2; i++) {
+//     console.log(age);
+// }
 
-function sayHello() {
-    console.log(age);
-}
-sayHello();
+// function sayHello() {
+//     console.log(age);
+// }
+// sayHello();
 
 //Inn sare tarike me chalega
+
+
+//Function scope -> keval { } ke ander hi acces kar skte hai variable ko
+function sayName() {
+    var fullName = "Vaibhav Dutta";
+    console.log("Enpower", fullName);
+}
+// console.log(fullName);
+// sayName();
+
+
+//Block scope
