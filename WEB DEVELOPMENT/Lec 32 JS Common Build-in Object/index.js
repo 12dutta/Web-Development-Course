9e1c8e94a72543ffecd49bb742b3689733883cf9
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-05-03 19:07:53 +0530

    Completed

--- WEB DEVELOPMENT/Lec 32 JS Common Build-in Object/index.js
+++ WEB DEVELOPMENT/Lec 32 JS Common Build-in Object/index.js
@@ -1,9 +1,23 @@
+//MATH Object
 // console.log(Math.PI);
 // console.log(Math.max(23,345,45,56));
 // console.log(Math.min(0,23,45,65,));
 
 // console.log(Math.round(2.6));
-console.log(Math.ceil(2.6)); //ceil just bda number dega
-console.log(Math.floor(2.6)); // floor just chota number dega
+// console.log(Math.ceil(2.6)); //ceil just bda number dega
+// console.log(Math.floor(2.6)); // floor just chota number dega
 
-console.log(Math.abs(-5));
+// console.log(Math.abs(-5));
+// console.log(Math.random()); //random 0 se 1 ke beech me koi bhi number dega
+// console.log(Math.sqrt(36)); //square root dega
+// console.log(Math.pow(5,2)); //power dega
+
+
+
+//DATE object
+// let curr = Date();
+let date = new Date('may 12 2014 07:24');
+// console.log(date);
+console.log(date.getFullYear());
+console.log(date.setFullYear(5621));
+console.log(date);
