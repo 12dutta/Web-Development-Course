71f31fb98aa56314aad525ca73e3c64c55d5f580
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-05-03 18:49:24 +0530

    Added

---
+++ WEB DEVELOPMENT/Lec 32 JS Common Build-in Object/index.js
@@ -0,0 +1,9 @@
+// console.log(Math.PI);
+// console.log(Math.max(23,345,45,56));
+// console.log(Math.min(0,23,45,65,));
+
+// console.log(Math.round(2.6));
+console.log(Math.ceil(2.6)); //ceil just bda number dega
+console.log(Math.floor(2.6)); // floor just chota number dega
+
+console.log(Math.abs(-5));
