3b484cb39204f38160ebc5e0cc8a989d7e8c8852
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-05-07 18:13:28 +0530

    Completed

--- WEB DEVELOPMENT/Lec 34 JS Error Handling/index.js
+++ WEB DEVELOPMENT/Lec 34 JS Error Handling/index.js
@@ -6,12 +6,29 @@
 
 
 //Handling-> try -catch block
-try {
-    console.log("Try block start here: ")
-    console.log(x); 
-}
-catch(e) {
-    //isme define karte hai ki, error ke saath ky karna chate hai.
-    console.log("I am inside the catch block");
-    console.log("Your error is here: ", e);
+// try {
+//     console.log("Try block start here: ")
+//     console.log(x); 
+// }
+// catch(e) {
+//     //isme define karte hai ki, error ke saath ky karna chate hai.
+//     console.log("I am inside the catch block");
+//     console.log("Your error is here: ", e);
+// }
+// finally{
+//     console.log("I will run everytime, as I am finally block:")
+// }
+
+
+//let create customize error
+// try{
+//     console.log(x);
+// }
+// catch(e) {
+//     throw new Error("Bhai phele declare toh kar,fir print karna:");
+// }
+
+let errorCode = 100;
+if(errorCode == 100) {
+    throw new Error("Invalid Json");
 }
