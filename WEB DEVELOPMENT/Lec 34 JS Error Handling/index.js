9e84ad70aab5b0deb4a0ff93ad981bc47ce5b883
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-05-07 17:20:35 +0530

    Added

--- WEB DEVELOPMENT/Lec 34 JS Error Handling/index.js
+++ WEB DEVELOPMENT/Lec 34 JS Error Handling/index.js
@@ -2,4 +2,15 @@
 // console.log(1;  //syntax error
 
 //runtime error
-console.log(x);  //undefined
+// console.log(x);  //undefined-> reference error
+
+
+//Handling-> try -catch block
+try {
+    console.log("Try block start here: ")
+    console.log(x); 
+}
+catch(e) {
+    console.log("I am inside the catch block");
+    console.log("Your error is here: ", e);
+}
