b943638b7e2af5583510f1c9871bd605da65284a
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-05-09 17:20:51 +0530

    completed

--- WEB DEVELOPMENT/Lec 36 JS Dom Manipulation/index.js
+++ WEB DEVELOPMENT/Lec 36 JS Dom Manipulation/index.js
@@ -1,6 +1,10 @@
 let mydiv = document.querySelector('#mydiv');
 
-let newElement = document.createElement('span');
-newElement.textContent = "Vaibhav Dutta";
+// let newElement = document.createElement('span');
+// newElement.textContent = "Vaibhav Dutta";
 
-mydiv.insertAdjacentElement('afterend', newElement);
+// mydiv.insertAdjacentElement('afterend', newElement);
+
+let parent = document.querySelector('#mydiv');
+let child = document.querySelector('#fpara');
+parent.removeChild(child);
